fade32d343a3c404abd7b9e504dc347410c2c2c7
alonsat <alonsatchi@gmail.com>
2022-11-18 21:40:46 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import xlrd
+
+PEOPLE_FILE = "people.xls"
+
+def main():
+    workbook = xlrd.open_workbook(PEOPLE_FILE)
+    worksheet = workbook.sheet_by_index(0)
+    first_row = [] # The row where we stock the name of the column
+    for col in range(worksheet.ncols):
+        first_row.append( worksheet.cell_value(0,col) )
+    # tronsform the workbook to a list of dictionnary
+    print(first_row)
+    data =[]
+    for row in range(1, worksheet.nrows):
+        elm = {}
+        for col in range(worksheet.ncols):
+            elm[first_row[col]]=worksheet.cell_value(row,col)
+        data.append(elm)
+    print(data)
+
+        
+
+if __name__ == "__main__":
+    main()
